172d7ccf2e166aa703c853181a44c79ad0df0f09
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-10-09 14:02:49 -0400

    Starting C++ version of MIPS project to read string and determine base-N value

---
+++ project1.cpp
@@ -0,0 +1,7 @@
+/*Written in C++ first since I am more familiar with it. This program will be converted into Assembly*/
+#include <iostream>
+#include string
+using namespace std;
+
+int main(){
+	string contestar;} //stores user input before conversion
