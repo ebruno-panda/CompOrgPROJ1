6dcb8b396fe0e993b44785da6e095e22f497bcac
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-10-09 15:53:49 -0400

    Error adding file 3 commits ago. Now adding project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -1,7 +1,14 @@
 /*Written in C++ first since I am more familiar with it. This program will be converted into Assembly*/
-#include <iostream>
-#include string
-using namespace std;
+#include <iostream> //(1)
+#include string //(2)
+using namespace std; //(3)
 
-int main(){
-	string contestar;} //stores user input before conversion
+int main(){ //(4)
+	string contestar; //(5) | stores user input
+	cin >> contestar; //(1)
+	
+	int N = 26 + (2935002 % 11); //(2) ID has a 0 in front | N = 26 + 4 | N = 30
+	int M = N - 10; //(3)
+
+	while(contestar.length() != 10){ //(4)
+		cout << "Sorry. Iput must be exactly 10 characters." << endl;}} //(5)
