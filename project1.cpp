942f62839ca3f86016e80006ee52f1f5a57ad243
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-10-12 20:01:31 -0400

    Using getline to count spaces and setting output to 0 if input length exceeds 10 characters

--- project1.cpp
+++ project1.cpp
@@ -5,18 +5,13 @@ using namespace std; //(3)
 
 int main(){ //(4)
 	string contestar; //(5) | stores user input
-	cin >> contestar; //(1)
-	
-	int N = 26 + (2935002 % 11); //(2) ID has a 0 in front | N = 26 + 4 | N = 30
-	int M = N - 10; //(3)
+	getline(cin, contestar); //(1)
 
-	while(contestar.length() != 10){ //(4)
-		cout << "Sorry. Input must be exactly 10 characters." << endl; //(5
-		cin >> contestar;} //(1)
-	for(int value = 0; value < 9; value++){//(2)
-   	 char actual = contestar[value]; //(3)
-   	 int dec = int(actual); //(4)
-   	 int holder; //(5)
+	int N = 26 + (2935002 % 11); //(2) ID has a 0 in front | N = 26 + 4 | base N = 30
 
-	
-	
+	/*Outputs 0 if input length > 10*/
+	if (contestar.length() > 10){cout << 0 << endl;} //(3)
+
+	else { //(4)
+	/*Will hold total of string base-N value*/
+	int record_says = 0; //(5)
