a1c0e20d14270f32dc9a29362bd6e37346571119
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-10-15 15:14:28 -0400

    Range with ASCII decimal letters of base30

--- project1.s
+++ project1.s
@@ -11,4 +11,11 @@
 		syscall
 		
 		
-	decstr2b30: # convert hex string to int 
+	decstr2b30: # convert hex string to int
+		add $t4, $zero, $zero
+		
+		
+		li $s4, 0x61 # $t4 = 'a' 
+		li $s5, 0x74 # $t7 = 't'
+		li $t4, 0x41 # $t4 = 'A' 
+		li $t5, 0x54 # $t7 = 'T' 
